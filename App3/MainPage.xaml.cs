e1effaf707834cf61d7466222dcf2ba3e687a8ae
Lass39 <45942786+Lass39@users.noreply.github.com>
2020-09-17 19:00:02 -0300

    Add files via upload

---
+++ App3/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void ButtonLogin_Clicked(object sender, EventArgs e)
+        {
+            var Id = EntryId.Text.ToString();
+            var Pwr = EntryPassword.Text.ToString();
+
+            if (Id == "admin" && Pwr == "senha@dmin")
+            {
+                DisplayAlert("Log in", "Sucesso!", "Voltar");
+            }
+            else
+            {
+                DisplayAlert("Log in", "Dados inválidos!", "Voltar");
+            }
+        }
+
+        private void ButtonLimpar_Clicked(object sender, EventArgs e)
+        {
+            EntryId.Text = "";
+            EntryPassword.Text = "";
+            EntryId.Focus();
+        }
+
+        private void ButtonCréditos_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Créditos", "Giovanni Fernandes 1640488", "Voltar");
+        }
+    }
+}
